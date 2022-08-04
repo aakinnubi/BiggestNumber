311e50ef8325ad36573fc3ff17e67eedd114deba
Abiola Akinnubi <abiola.akinnubi@activision.com>
2022-08-04 01:47:18 -0500

    variable fixes

--- BiggestNumber.cpp
+++ BiggestNumber.cpp
@@ -10,22 +10,13 @@ int main()
     DisplayLargest(arrayData);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
 
 int LargestNumber(int arrayData[])
 {
-    if (sizeof(arrayData) > 0) {
+    int size = sizeof(arrayData);
+    if (size> 0) {
         int currentLargest = arrayData[0];
-        for (int i = 0; i < sizeof(arrayData); i++)
+        for (int i = 0; i < size; i++)
         {
             if (currentLargest < arrayData[i]) {
                 currentLargest = arrayData[i];
